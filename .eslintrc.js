e8cf0ea4746301be0970bb84f17acef45fa1acfe
kmkzt <info.pscreator@gmail.com>
2020-02-07 15:22:59 +0900

    fix: fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -38,18 +38,10 @@ module.exports = {
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
     strict: 'error',
-    'check-constructor': false,
-    'no-comma-dangle': false,
-    'no-empty-interface': false,
+    'check-constructor': [0],
+    'no-comma-dangle': [0],
+    'no-empty-interface': [0],
     'no-unused-vars': [0],
-    'prettier/prettier': [
-      'error',
-      {
-        semi: false,
-        singleQuote: true,
-        printWidth: 80,
-        tabWidth: 2
-      }
-    ]
+    'prettier/prettier': ['error']
   }
 }
