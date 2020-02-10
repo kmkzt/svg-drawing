6842267fb4d0cf16a7d0adc2e15c8539106869d7
kmkzt <info.pscreator@gmail.com>
2020-02-10 14:35:31 +0900

    fix: fix babel config

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,16 @@
-module.exports = {
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        targets: {
-          node: 'current'
+module.exports = api => {
+  api.cache(process.env.NODE_ENV === 'development')
+  return {
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          targets: {
+            node: 'current'
+          }
         }
-      }
-    ],
-    '@babel/preset-typescript'
-  ]
+      ],
+      '@babel/preset-typescript'
+    ]
+  }
 }
