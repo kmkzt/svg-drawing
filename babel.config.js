b849f9d0d7d6f89ef0a2876c3166e2860dfc2e7e
kmkzt <info.pscreator@gmail.com>
2020-03-12 23:28:50 +0900

    feat: update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,31 @@
+const { BABEL_ENV, NODE_ENV } = process.env
+const modules = BABEL_ENV === 'cjs' || NODE_ENV === 'test' ? 'commonjs' : false
+
 module.exports = api => {
-  api.cache(process.env.NODE_ENV === 'development')
+  api.cache(false)
   return {
     presets: [
       [
         '@babel/preset-env',
         {
-          targets: {
-            node: 'current'
-          }
+          targets: NODE_ENV === 'test' ? { node: 'current' } : undefined,
+          loose: true,
+          modules
         }
       ],
+      // react only
+      // '@babel/preset-react',
       '@babel/preset-typescript'
+    ],
+    plugins: [
+      // react only
+      // [
+      //   'babel-plugin-transform-react-remove-prop-types',
+      //   { mode: 'unsafe-wrap' }
+      // ],
+      ['@babel/plugin-proposal-object-rest-spread', { loose: true }],
+      ['@babel/plugin-proposal-class-properties', { loose: true }]
+      // modules === 'commonjs' && 'add-module-exports'
     ]
   }
 }
