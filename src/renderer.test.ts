b4d0cd213daff742fee1aee79a45f6d5febdee7b
kmkzt <info.pscreator@gmail.com>
2020-05-10 23:02:06 +0900

    feat: add SvgPath class

--- src/renderer.test.ts
+++ src/renderer.test.ts
@@ -1,4 +1,4 @@
-import { Point, Vector } from './renderer'
+import { Point, Vector, SvgPath } from './renderer'
 
 describe('renderer', () => {
   describe('Point', () => {
@@ -25,21 +25,6 @@ describe('renderer', () => {
       expect(vec.value).toBe(1.4142135623730951)
       expect(vec.angle).toBe(0.7853981633974483)
     })
-    // // command
-    // {
-    //     // move
-    //     assert_eq!(Point { x: 1.0, y: 1.0 }.command_move(), "M 1 1");
-    //     assert_eq!(Point { x: 1.1, y: 1.1 }.command_move(), "M 1.1 1.1");
-    //     // line
-    //     assert_eq!(Point { x: 1.0, y: 1.0 }.command_line(), " L 1 1");
-    //     assert_eq!(Point { x: 1.1, y: 1.1 }.command_line(), " L 1.1 1.1");
-    //     // Circuler
-    //     assert_eq!(
-    //         Point { x: 1.0, y: 1.0 }
-    //             .command_circuler(&Point { x: 0.2, y: 1.2 }, &Point { x: 0.8, y: 1.2 }),
-    //         " C 0.2 1.2 0.8 1.2 1 1"
-    //     );
-    // }
   })
   describe('Vector', () => {
     it('toPoint', () => {
@@ -51,6 +36,65 @@ describe('renderer', () => {
       const vec = new Vector(1.0, 0.5).scale(0.3)
       expect(vec.value).toBe(0.3)
       expect(vec.angle).toBe(0.5)
+    })
+  })
+  describe('SvgPath', () => {
+    it('addPoint', () => {
+      const path = new SvgPath()
+      path.addPoint(new Point(1, 1))
+      expect(path.data.length).toBe(1)
+      expect(path.data[0].x).toBe(1)
+      expect(path.data[0].y).toBe(1)
+    })
+    it('scale', () => {
+      const path = new SvgPath({ strokeWidth: 1 })
+      path.addPoint(new Point(1, 1))
+      path.scale(2)
+      expect(path.strokeWidth).toBe(2)
+      expect(path.data[0].x).toBe(2)
+      expect(path.data[0].y).toBe(2)
+    })
+    it('createCommand Line', () => {
+      const path = new SvgPath({ circuler: false, close: false })
+      path.addPoint(new Point(0, 0))
+      path.addPoint(new Point(1, 1))
+      path.addPoint(new Point(-1, -1))
+      expect(path.createCommand()).toBe('M 0 0 L 1 1 L -1 -1')
+    })
+    it('createCommand Line Close', () => {
+      const path = new SvgPath({ circuler: false, close: true })
+      path.addPoint(new Point(0, 0))
+      path.addPoint(new Point(1, 1))
+      path.addPoint(new Point(-1, -1))
+      expect(path.createCommand()).toBe('M 0 0 L 1 1 L -1 -1 L 0 0 Z')
+    })
+    it('createCommand Circuler', () => {
+      const path = new SvgPath({ circuler: true, close: false })
+      path.addPoint(new Point(0, 0))
+      path.addPoint(new Point(1, 1))
+      path.addPoint(new Point(2, 1))
+      path.addPoint(new Point(3, 0))
+      expect(path.createCommand()).toBe(
+        'M 0 0 C 0.2 0.2 0.6 0.8 1 1 C 1.4 1.2 1.6 1.2 2 1 C 2.4 0.8 2.8 0.2 3 0'
+      )
+    })
+    it('createCommand Circuler Close', () => {
+      const path = new SvgPath({ circuler: true, close: true })
+      path.addPoint(new Point(0, 0))
+      path.addPoint(new Point(1, 1))
+      path.addPoint(new Point(2, 1))
+      path.addPoint(new Point(3, 0))
+      expect(path.createCommand()).toBe(
+        'M 0 0 C 0.2 0.2 0.6 0.8 1 1 C 1.4 1.2 1.6 1.2 2 1 C 2.4 0.8 2.8 0.2 3 0 C 2.6 -0.2 0.4 -0.2 0 0 Z'
+      )
+    })
+    it('toElement', () => {
+      const path = new SvgPath({ circuler: true, close: false })
+      path.addPoint(new Point(0, 0))
+      path.addPoint(new Point(1, 1))
+      path.addPoint(new Point(2, 1))
+      path.addPoint(new Point(3, 0))
+      expect(path.toElement()).toMatchSnapshot()
     })
   })
 })
--- src/renderer.ts
+++ src/renderer.ts
@@ -29,6 +29,18 @@ export class Point {
   public sub(p: Point): Point {
     return new Point(this.x - p.x, this.y - p.y)
   }
+
+  public command_move() {
+    return `M ${this.x} ${this.y}`
+  }
+
+  public command_line() {
+    return ` L ${this.x} ${this.y}`
+  }
+
+  public command_circuler(l: Point, r: Point) {
+    return ` C ${l.x} ${l.y} ${r.x} ${r.y} ${this.x} ${this.y}`
+  }
 }
 
 export class Vector {
@@ -47,5 +59,113 @@ export class Vector {
 
   public scale(r: number): Vector {
     return new Vector(this.value * r, this.angle)
+  }
+}
+
+interface SvgPathOption {
+  close?: boolean
+  circuler?: boolean
+  strokeWidth?: number
+  stroke?: string
+  fill?: string
+  background?: string
+}
+export class SvgPath {
+  public close: boolean
+  public circuler: boolean
+  public strokeWidth: number
+  public stroke: string
+  public fill: string
+  public background: string
+  public smoothRatio: number
+  public data: Point[]
+
+  constructor({
+    close,
+    circuler,
+    strokeWidth,
+    stroke,
+    fill
+  }: SvgPathOption = {}) {
+    this.close = close ?? false
+    this.circuler = circuler ?? true
+    this.strokeWidth = strokeWidth ?? 1
+    this.stroke = stroke ?? '#000'
+    this.fill = fill ?? 'none'
+    this.background = this.background ?? '#fff'
+    this.smoothRatio = 0.2
+    this.data = []
+  }
+
+  public scale(r: number) {
+    const update = this.data.map((p: Point) => p.scale(r))
+    this.data = update
+    this.strokeWidth *= r
+  }
+
+  public addPoint(p: Point) {
+    this.data.push(p)
+  }
+
+  public createCommand(): string {
+    if (this.data.length === 0) return ''
+    const createControlPoint = (
+      prev: Point,
+      start: Point,
+      next: Point
+    ): Point => {
+      const contolVectorPoint = next
+        .sub(prev)
+        .toVector()
+        .scale(this.smoothRatio)
+        .toPoint()
+      return start.add(contolVectorPoint)
+    }
+    let command = this.data[0].command_move()
+    const isCirculer = this.circuler && this.data.length > 2
+    const endIndex = this.data.length - 1
+    if (isCirculer) {
+      for (let i = 1; i < this.data.length; i += 1) {
+        const p1 = i === 1 ? this.data[0] : this.data[i - 2]
+        const p2 = this.data[i - 1]
+        const p3 = this.data[i]
+        const p4 = i === endIndex ? this.data[i] : this.data[i + 1]
+        command += this.data[i].command_circuler(
+          createControlPoint(p1, p2, p3),
+          createControlPoint(p4, p3, p2)
+        )
+      }
+    } else {
+      for (let i = 1; i < this.data.length; i += 1) {
+        command += this.data[i].command_line()
+      }
+    }
+    if (this.close) {
+      if (isCirculer) {
+        command += this.data[0].command_circuler(
+          createControlPoint(
+            this.data[endIndex - 1],
+            this.data[endIndex],
+            this.data[0]
+          ),
+          createControlPoint(this.data[1], this.data[0], this.data[endIndex])
+        )
+      } else {
+        command += this.data[0].command_line()
+      }
+      command += ` Z`
+    }
+    return command
+  }
+
+  public toElement(): HTMLElement {
+    const path = document.createElement('path')
+    path.setAttribute('stroke-width', String(this.strokeWidth))
+    path.setAttribute('stroke', this.stroke)
+    path.setAttribute('fill', this.fill)
+    path.setAttribute('stroke-linejoin', this.circuler ? 'round' : 'mitter')
+    path.setAttribute('stroke-linecap', this.circuler ? 'round' : 'square')
+    path.setAttribute('d', this.createCommand())
+    return path
   }
 }
