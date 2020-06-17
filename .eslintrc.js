b16142cf111a300175b0d03a1168565b0184380c
kmkzt <info.pscreator@gmail.com>
2020-06-17 22:42:43 +0900

    chore: update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,47 +1,48 @@
 module.exports = {
-  root: true,
   env: {
     browser: true,
     commonjs: true,
     es6: true
   },
-  extends: ['plugin:react/recommended', 'prettier/@typescript-eslint'],
-  plugins: ['@typescript-eslint', 'prettier', 'react', 'react-hooks'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+    'plugin:jsx-a11y/recommended',
+    'prettier/babel',
+    'prettier/react',
+    'prettier/@typescript-eslint'
+  ],
   parser: '@typescript-eslint/parser',
-  parserOptions: {
-    sourceType: 'module',
-    ecmaVersion: 2018,
-    ecmaFeatures: {
-      jsx: true
-    }
-  },
   settings: {
     react: {
-      createClass: 'createReactClass',
-      pragma: 'React', // Pragma to use, default to "React"
-      version: '16.8'
-    },
-    propWrapperFunctions: [
-      'forbidExtraProps',
-      { property: 'freeze', object: 'Object' },
-      { property: 'myFavoriteWrapper' }
-    ],
-    linkComponents: ['Hyperlink', { name: 'Link', linkAttribute: 'to' }]
+      version: 'detect'
+    }
   },
   rules: {
-    // react-config -> https://github.com/yannickcr/eslint-plugin-react/
+    strict: 'error',
+    'check-constructor': 'off',
+    'no-comma-dangle': 'off',
+    'no-empty-interface': 'off',
+    'no-unused-vars': 'off',
+
     'react/jsx-uses-react': 'error',
     'react/jsx-uses-vars': 'error',
     'react/no-deprecated': 'error',
-    'react/display-name': [0],
-    'react/prop-types': [0],
+    'react/display-name': 'off',
+    'react/prop-types': 'off',
+
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
-    strict: 'error',
-    'check-constructor': [0],
-    'no-comma-dangle': [0],
-    'no-empty-interface': [0],
-    'no-unused-vars': [0],
-    'prettier/prettier': ['error']
+
+    'jsx-a11y/click-events-have-key-events': 'off',
+    'jsx-a11y/no-static-element-interactions': 'off',
+
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-use-before-define': 'off'
   }
 }
