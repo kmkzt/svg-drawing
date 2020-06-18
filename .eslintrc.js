45ee18ee8f87ccb9656e5edcc959ab333f735efb
kmkzt <info.pscreator@gmail.com>
2020-06-18 12:59:53 +0900

    chore: add eslint-plugin-jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
     'plugin:react-hooks/recommended',
     'plugin:prettier/recommended',
     'plugin:jsx-a11y/recommended',
+    'plugin:jest/recommended',
     'prettier/babel',
     'prettier/react',
     'prettier/@typescript-eslint'
