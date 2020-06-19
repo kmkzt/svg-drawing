bb07c28b10d4719cb9ea923b658a6b9416af8b98
kmkzt <info.pscreator@gmail.com>
2020-06-20 00:20:14 +0900

    feat: add toAnimationSvgXML methods

--- src/animation.test.ts
+++ src/animation.test.ts
@@ -8,26 +8,45 @@ const defaultTestData = `<svg width="200" height="200">
 describe('animation.ts', () => {
   describe('SvgAnimation', () => {
     const generateAniamtion = (svgStr = defaultTestData) => {
-      const animation = new SvgAnimation(document.createElement('div'))
-      animation.parseSVGString(svgStr)
-      return animation
+      const svg = new SvgAnimation(document.createElement('div'))
+      svg.parseSVGString(svgStr)
+      return svg
     }
     it('init', () => {
       expect(generateAniamtion()).toMatchSnapshot()
     })
-    it('generateFrame', () => {
-      const anim = generateAniamtion()
-      anim.setAnimation(paths => {
+    it('generateFrame restore', () => {
+      const svg = generateAniamtion()
+      svg.setAnimation(paths => {
         return [paths[0]]
       })
-      expect(anim.generateFrame().length).toBe(1)
+      expect(svg.generateFrame().length).toBe(1)
     })
     it('totalCommandsLength', () => {
-      const animation = generateAniamtion(`<svg width="200" height="200">
+      const svg = generateAniamtion(`<svg width="200" height="200">
         <path fill="#f00" stroke="#00f" stroke-width="4" d="M 1 1 L 2 2 C 3 3 5 3 7 3 Z"></path>
         <path fill="#ff0" stroke="#f0f" stroke-width="2" d="M 2 2 L 4 4 C 6 6 10 6 14 6 Z"></path>
       </svg>`)
-      expect(animation.totalCommandsLength).toBe(8)
+      expect(svg.totalCommandsLength).toBe(8)
+    })
+
+    it('toAnimationSvgXML', () => {
+      const svg = generateAniamtion()
+      const testAnimation: FrameAnimation = (paths, count) => {
+        const update = []
+        for (let i = 0; i < paths.length; i += 1) {
+          if (count < paths[i].commands.length) {
+            paths[i].commands = paths[i].commands.slice(0, count)
+            update.push(paths[i])
+            break
+          }
+          count -= paths[i].commands.length
+          update.push(paths[i])
+        }
+        return update
+      }
+      svg.setAnimation(testAnimation)
+      expect(svg.toAnimationSvgXML()).toMatchSnapshot()
     })
   })
 })
--- src/animation.ts
+++ src/animation.ts
@@ -9,7 +9,7 @@ export class SvgAnimation extends Renderer {
   private _stopId: number
   private _stop: (() => void) | null
   private _anim: FrameAnimation | null
-  private _restorePath: Path[]
+  private _restorePaths: Path[]
   constructor(
     el: HTMLElement,
     { background, ms }: AnimationOption = { ms: 60 }
@@ -18,7 +18,7 @@ export class SvgAnimation extends Renderer {
     this.ms = ms
     this._stop = null
     this._anim = null
-    this._restorePath = []
+    this._restorePaths = []
     this._stopId = 0
   }
 
@@ -33,14 +33,14 @@ export class SvgAnimation extends Renderer {
   }
 
   public restore() {
-    this.replacePaths(this._restorePath)
+    this.replacePaths(this._restorePaths)
     this.update()
   }
 
   public generateFrame(index?: number): Path[] {
     if (!this._anim) return this.paths
     return this._anim(
-      this._restorePath.map(p => p.clone()),
+      this._restorePaths.map(p => p.clone()),
       index
     )
   }
@@ -54,7 +54,7 @@ export class SvgAnimation extends Renderer {
     let index = 0
     this.stop()
     const ms = this.ms
-    this._restorePath = this.clonePaths()
+    this._registerPaths()
     let start: number | undefined
     const frame: FrameRequestCallback = timestamp => {
       if (ms !== this.ms) {
@@ -75,5 +75,61 @@ export class SvgAnimation extends Renderer {
       cancelAnimationFrame(this._stopId)
       this._stop = null
     }
+  }
+
+  public toAnimationSvgXML() {
+    this._registerPaths()
+    if (!this._restorePaths) return
+    const animPathsList: Path[][] = []
+    for (let i = 0; i < this.totalCommandsLength; i += 1) {
+      animPathsList.push(this.generateFrame(i))
+    }
+    const dur = this.totalCommandsLength * this.ms + 'ms'
+    const t = 1 / this.totalCommandsLength
+    const keyTimes = `0;${Array(this.totalCommandsLength)
+      .fill(undefined)
+      .map((_, i) => i * t + '')
+      .join(';')};1`
+    const animPaths = this._restorePaths.map(p => {
+      const aEl = document.createElementNS(
+        'http://www.w3.org/2000/svg',
+        'animate'
+      )
+      aEl.setAttribute('attributeName', 'd')
+      aEl.setAttribute('repeatCount', 'indefinite')
+      aEl.setAttribute('dur', dur)
+      aEl.setAttribute('keyTimes', keyTimes)
+
+      const dList = animPathsList.map(animPaths => {
+        const path = animPaths.find(ap => ap.attrs.id === p.attrs.id)
+        return path && path.commands.length > 0
+          ? path.getCommandString()
+          : p.commands[0].toString()
+      })
+
+      aEl.setAttribute(
+        'values',
+        [p.commands[0].toString(), ...dList, p.getCommandString()].join(';')
+      )
+
+      const pEl = p.toElement()
+      pEl.appendChild(aEl)
+      return pEl
+    })
+    const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+    svg.setAttributeNS(null, 'version', '1.1')
+    svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg')
+    svg.setAttribute('xmlns:xlink', 'http://www.w3.org/1999/xlink')
+    svg.setAttribute('width', String(this.width))
+    svg.setAttribute('height', String(this.height))
+    animPaths.map(el => svg.appendChild(el))
+    return svg
+  }
+
+  private _registerPaths() {
+    this._restorePaths = this.clonePaths().map((p, i) => {
+      p.attrs.id = `t${i}`
+      return p
+    })
   }
 }
