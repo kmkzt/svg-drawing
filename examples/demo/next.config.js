1c95ee78018337515fd545aca8d8420fddf4269a
kmkzt <info.pscreator@gmail.com>
2020-07-27 14:49:24 +0900

    fix: fix build config for demo

--- examples/demo/next.config.js
+++ examples/demo/next.config.js
@@ -1,4 +1,7 @@
+const isProd = process.env.NODE_ENV === 'production'
 module.exports = {
+  // For gh-pages
+  assetPrefix: isProd ? '/svg-drawing' : '',
   webpack(config, { dev }) {
     if (dev) {
       config.module.rules.push({
