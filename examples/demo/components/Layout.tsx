1453f5bbcf7141589b3fc62ffc7eabb45bb69f25
kmkzt <info.pscreator@gmail.com>
2020-07-28 22:46:14 +0900

    fix: fix meta

--- examples/demo/components/Layout.tsx
+++ examples/demo/components/Layout.tsx
@@ -17,11 +17,6 @@ const Layout: React.SFC<{
     <Fragment>
       <Head>
         <title>{`svg-drawing ${title}`}</title>
-        <meta charSet="utf-8" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, user-scalable=no"
-        />
       </Head>
       <GlobalStyle />
       <Header>
