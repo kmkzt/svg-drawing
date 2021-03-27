5827e482539e3ffd4e5fbd1ca0f2c66e9d03db76
kmkzt <info.pscreator@gmail.com>
2021-03-27 21:40:59 +0900

    feat: add resize handler and improve rendering timing

--- packages/react/src/useDrawingUnstable.tsx
+++ packages/react/src/useDrawingUnstable.tsx
@@ -10,10 +10,12 @@ import { Svg, Path, Command, COMMAND_TYPE } from '@svg-drawing/core/lib/svg'
 import { DrawHandler, ResizeHandler } from '@svg-drawing/core/lib/handler'
 import { Convert } from '@svg-drawing/core/lib/convert'
 import { throttle } from '@svg-drawing/core/lib/throttle'
+import { isAlmostSameNumber } from '@svg-drawing/core/lib/utils'
 import type {
   DrawHandlerCallback,
   DrawingOption,
   PointObject,
+  ResizeHandlerCallback,
   SvgObject,
   SvgOption,
 } from '@svg-drawing/core/lib/types'
@@ -27,6 +29,8 @@ type UseDrawingUnstable<T extends HTMLElement = any> = [
 // eslint-disable-next-line @typescript-eslint/ban-types
 type DrawingMethods = {}
 
+const RENDER_INTERVAL = 0
+
 export const useDrawingUnstable = <T extends HTMLElement>({
   penColor,
   penWidth,
@@ -37,14 +41,13 @@ export const useDrawingUnstable = <T extends HTMLElement>({
   background,
 }: DrawingOption & Pick<SvgOption, 'background'>): UseDrawingUnstable<T> => {
   const drawElRef = useRef<T>(null)
-  const drawHandlerRef = useRef<DrawHandler | null>(null)
-  const resizeHandlerRef = useRef<ResizeHandler | null>(null)
+  const svgRef = useRef(new Svg({ width: 0, height: 0 }))
   const drawPathRef = useRef<Path | null>(null)
   const drawPointsRef = useRef<PointObject[]>([])
-
-  const svg = useMemo(() => new Svg({ width: 0, height: 0 }), [])
   const convert = useMemo(() => new Convert(), [])
-  const [svgObj, setSvgObj] = useState(svg.toJson())
+
+  const shouldUpdateRef = useRef<boolean>(false)
+  const [svgObj, setSvgObj] = useState(svgRef.current.toJson())
 
   const createDrawPath = useCallback((): Path => {
     drawPointsRef.current = []
@@ -57,7 +60,7 @@ export const useDrawingUnstable = <T extends HTMLElement>({
     })
   }, [penColor, penWidth, fill, curve])
 
-  const updateCommands = useCallback(() => {
+  const updateSvg = useCallback(() => {
     if (!drawPathRef.current) return
     if (curve) {
       drawPathRef.current.commands = convert.bezierCurveCommands(
@@ -66,78 +69,104 @@ export const useDrawingUnstable = <T extends HTMLElement>({
     } else {
       drawPathRef.current.commands = convert.lineCommands(drawPointsRef.current)
     }
-
-    console.log('close', close)
     if (close) {
       drawPathRef.current.commands.push(new Command(COMMAND_TYPE.CLOSE))
     }
 
-    setSvgObj(svg.toJson())
-  }, [curve, close, svg, convert])
+    shouldUpdateRef.current = true
+  }, [curve, close, convert])
   const handleDrawStart = useCallback<DrawHandlerCallback['start']>(() => {
     if (drawPathRef.current) return
     drawPathRef.current = createDrawPath()
-    svg.addPath(drawPathRef.current)
-  }, [svg, createDrawPath])
+    svgRef.current.addPath(drawPathRef.current)
+  }, [createDrawPath])
 
-  const handleDrawMove = useCallback<DrawHandlerCallback['move']>(
-    (po) => {
-      drawPointsRef.current.push(po)
-      updateCommands()
-    },
-    [updateCommands]
-  )
+  const handleDrawMove = useMemo<DrawHandlerCallback['move']>(() => {
+    const move: DrawHandlerCallback['move'] = (po) => {
+      if (!drawPathRef.current) return
+      drawPointsRef.current = [...drawPointsRef.current, po]
+      updateSvg()
+    }
+    return throttle(move, delay ?? 0)
+  }, [delay, updateSvg])
+
   const handleDrawEnd = useCallback<DrawHandlerCallback['end']>(() => {
     drawPathRef.current = null
   }, [])
-  const handleThrottleDrawMove = useMemo(
-    () => throttle(handleDrawMove, delay ?? 0),
-    [delay, handleDrawMove]
-  )
 
-  useEffect(() => {
-    if (!drawHandlerRef.current) return
-    drawHandlerRef.current.move = handleThrottleDrawMove
-  }, [handleThrottleDrawMove])
-
+  // FIX: Create path while drawing
   // useEffect(() => {
   //   if (!drawPathRef.current) return
-  //   if (curve) {
-  //     drawPathRef.current.commands = convert.bezierCurveCommands(
-  //       drawPointsRef.current
-  //     )
-  //   } else {
-  //     drawPathRef.current.commands = convert.lineCommands(drawPointsRef.current)
-  //   }
+  //   if (
+  //     drawPathRef.current.attrs.strokeWidth &&
+  //     +drawPathRef.current.attrs.strokeWidth === penWidth &&
+  //     drawPathRef.current.attrs.stroke === penColor
+  //   )
+  //     return
 
-  //   if (close) {
-  //     drawPathRef.current.commands.push(new Command(COMMAND_TYPE.CLOSE))
-  //   }
+  //   const po = drawPointsRef.current[drawPointsRef.current.length - 1]
+  //   if (!po) return
+  //   drawPathRef.current = createDrawPath()
+  //   drawPointsRef.current = [po]
+  //   svgRef.current.addPath(drawPathRef.current)
+  //   updateSvg()
+  // }, [updateSvg, penWidth, penColor, createDrawPath])
 
-  //   setSvgObj(svg.toJson())
-  // }, [curve, close, convert])
+  /**
+   * Setup DrawHandler
+   */
+  const drawHandlerRef = useRef<DrawHandler | null>(null)
 
   useEffect(() => {
     if (!drawHandlerRef.current) return
     drawHandlerRef.current.start = handleDrawStart
-  }, [handleDrawStart])
+    drawHandlerRef.current.move = handleDrawMove
+    drawHandlerRef.current.end = handleDrawEnd
+  }, [handleDrawStart, handleDrawMove, handleDrawEnd])
+
   useEffect(() => {
     if (!drawElRef.current) return
     if (drawHandlerRef.current) return
-
     const { width, height } = drawElRef.current.getBoundingClientRect()
-    svg.resize({ width, height })
+    svgRef.current.resize({ width, height })
     drawHandlerRef.current = new DrawHandler(drawElRef.current, {
       start: handleDrawStart,
-      move: handleThrottleDrawMove,
+      move: handleDrawMove,
       end: handleDrawEnd,
     })
     drawHandlerRef.current.on()
-    // return () => {
-    //   if (drawHandlerRef.current) drawHandlerRef.current.off()
-    // }
   })
-  return [drawElRef, { ...svgObj, background }, {}]
+
+  /**
+   * Setup ResizeHandler
+   */
+  const resizeHandlerRef = useRef<ResizeHandler | null>(null)
+  useEffect(() => {
+    if (!drawElRef.current) return
+    if (resizeHandlerRef.current) return
+    resizeHandlerRef.current = new ResizeHandler(drawElRef.current, {
+      resize: ({ width, height }) => {
+        if (isAlmostSameNumber(svgRef.current.width, width)) return
+        svgRef.current.resize({ width, height })
+        shouldUpdateRef.current = true
+      },
+    })
+    resizeHandlerRef.current.on()
+  })
+
+  /**
+   * A variable called shouldUpdateRef manages whether to update to reduce the number of times setState is executed.
+   */
+  useEffect(() => {
+    const stopId = setInterval(() => {
+      if (!shouldUpdateRef.current) return
+      shouldUpdateRef.current = false
+      setSvgObj(svgRef.current.toJson())
+    }, RENDER_INTERVAL)
+    return () => clearInterval(stopId)
+  }, [])
+
+  return [drawElRef, svgObj, {}]
 }
 
 export const RenderSvg = ({ background, paths, ...size }: SvgObject) => (
