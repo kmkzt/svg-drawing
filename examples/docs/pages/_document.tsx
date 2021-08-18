6d7306f2d7710a1584952c77f20958857ed04ef0
kmkzt <info.pscreator@gmail.com>
2021-08-19 01:34:26 +0900

    fix: fix _app.js

--- examples/docs/pages/_document.tsx
+++ examples/docs/pages/_document.tsx
@@ -39,12 +39,6 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en-US">
         <Head>
-          <meta charSet="utf-8" />
-          <meta
-            key="viewport"
-            name="viewport"
-            content="width=device-width, initial-scale=1.0, user-scalable=no"
-          />
           {/* Global Site Tag (gtag.js) - Google Analytics */}
           <script
             async
