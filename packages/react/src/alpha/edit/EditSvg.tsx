45a30bf6404414d365f0e54bc21705515d258196
kmkzt <info.pscreator@gmail.com>
2021-09-12 23:36:41 +0900

    feat: added EditBoundingBox

--- packages/react/src/alpha/edit/EditSvg.tsx
+++ packages/react/src/alpha/edit/EditSvg.tsx
@@ -5,12 +5,7 @@ import React, {
   useEffect,
   HTMLAttributes,
 } from 'react'
-import {
-  PathObject,
-  PointObject,
-  ControlPoint,
-  FixedPositionType,
-} from '@svg-drawing/core'
+import { PointObject, FixedPositionType, BoundingBox } from '@svg-drawing/core'
 import { EditSvgProps } from '../types'
 
 type EditPointIndex = {
@@ -32,24 +27,14 @@ export const EditSvg = ({
   onSelecting: handleSelect,
   onMovePaths: handleMove,
   onMovePathsPreview: handleMovePreview,
-  onResizePaths: handleResizeEdit,
-  onResizePathsPreview: handleResizeEditPreview,
+  onResizePaths,
+  onResizePathsPreview,
   ...rest
 }: EditSvgProps & HTMLAttributes<SVGSVGElement>) => {
+  /** @todo Move to useEdit. */
   const [currentPosition, setCurrentPosition] = useState<PointObject | null>(
     null
   )
-
-  const [resizeBasePoint, setResizeBasePoint] =
-    useState<ResizeBasePoint | null>(null)
-
-  const isSelectedBoundingBox = useMemo(() => {
-    if (Object.keys(selecting).length < 1) return false
-    return Object.keys(selecting).every((pKey: string) => {
-      const selectingCommand = selecting[+pKey]
-      return Object.keys(selectingCommand).length === 0
-    })
-  }, [selecting])
 
   const isSelectedPoint = useCallback(
     ({ path, command, point }: EditPointIndex): boolean => {
@@ -60,6 +45,14 @@ export const EditSvg = ({
     },
     [selecting]
   )
+
+  const selectedBoundingBox = useMemo(() => {
+    if (Object.keys(selecting).length < 1) return false
+    return Object.keys(selecting).every((pKey: string) => {
+      const selectingCommand = selecting[+pKey]
+      return Object.keys(selectingCommand).length === 0
+    })
+  }, [selecting])
 
   const handleMoveStart = useCallback(
     (
@@ -130,54 +123,6 @@ export const EditSvg = ({
     [currentPosition, handleMove]
   )
 
-  const handleResizeStart = useCallback(
-    (type: FixedPositionType) =>
-      (
-        ev:
-          | React.MouseEvent<SVGRectElement | SVGPathElement | SVGCircleElement>
-          | React.TouchEvent<SVGRectElement | SVGPathElement | SVGCircleElement>
-      ) => {
-        ev.preventDefault()
-        const { x, y } = getPointFromEvent(ev)
-        setResizeBasePoint({
-          type,
-          x,
-          y,
-        })
-      },
-    []
-  )
-
-  const handleResizePreview = useCallback(
-    (ev: MouseEvent | TouchEvent) => {
-      if (!resizeBasePoint) return
-      const { x, y } = getPointFromEvent(ev)
-      const { type, ...base } = resizeBasePoint
-
-      handleResizeEditPreview(type, {
-        x: x - base.x,
-        y: y - base.y,
-      })
-    },
-    [handleResizeEditPreview, resizeBasePoint]
-  )
-
-  // resize edit
-  const handleResizeEnd = useCallback(
-    (ev: MouseEvent | TouchEvent) => {
-      if (!resizeBasePoint) return
-      const { x, y } = getPointFromEvent(ev)
-      const { type, ...base } = resizeBasePoint
-
-      handleResizeEdit(type, {
-        x: x - base.x,
-        y: y - base.y,
-      })
-      setResizeBasePoint(null)
-    },
-    [handleResizeEdit, resizeBasePoint]
-  )
-
   useEffect(() => {
     // move
     addEventListener('mouseup', handleMoveEnd)
@@ -187,14 +132,6 @@ export const EditSvg = ({
     addEventListener('mousemove', handleMoveEdit)
     addEventListener('touchmove', handleMoveEdit)
 
-    // resizeEdit
-    addEventListener('mouseup', handleResizeEnd)
-    addEventListener('touchcancel', handleResizeEnd)
-
-    // resizePreview
-    addEventListener('mousemove', handleResizePreview)
-    addEventListener('touchmove', handleResizePreview)
-
     return () => {
       // move
       removeEventListener('mouseup', handleMoveEnd)
@@ -203,35 +140,21 @@ export const EditSvg = ({
       // movePreview
       removeEventListener('mousemove', handleMoveEdit)
       removeEventListener('touchmove', handleMoveEdit)
-
-      // resizeEdit
-      removeEventListener('mouseup', handleResizeEnd)
-      removeEventListener('touchcancel', handleResizeEnd)
-
-      // resizePreview
-      removeEventListener('mousemove', handleResizePreview)
-      removeEventListener('touchmove', handleResizePreview)
     }
-  }, [handleMoveEnd, handleMoveEdit, handleResizeEnd, handleResizePreview])
+  }, [handleMoveEnd, handleMoveEdit])
 
   return (
     <svg width={width} height={height} {...rest}>
       {background && <rect width={width} height={height} fill={background} />}
-      <rect
-        x={boundingBox.min[0]}
-        y={boundingBox.min[1]}
-        width={boundingBox.max[0] - boundingBox.min[0]}
-        height={boundingBox.max[1] - boundingBox.min[1]}
-        stroke={EDIT_CONFIG.color.main}
-        fill={
-          isSelectedBoundingBox
-            ? EDIT_CONFIG.fill.selected
-            : EDIT_CONFIG.fill.boundingBox
-        }
-        onMouseDown={handleMoveStart}
-        onTouchStart={handleMoveStart}
+      <EditBoundingBox
+        selected={selectedBoundingBox}
+        min={boundingBox.min}
+        max={boundingBox.max}
+        onMoveStart={handleMoveStart}
+        onResizePaths={onResizePaths}
+        onResizePathsPreview={onResizePathsPreview}
       />
-      {paths.map((pathAttr: PathObject, pathIndex) => (
+      {paths.map((pathAttr, pathIndex) => (
         <path
           key={pathIndex}
           {...pathAttr}
@@ -252,7 +175,7 @@ export const EditSvg = ({
               onMouseDown={handleMoveStartPath(pathIndex)}
               onTouchStart={handleMoveStartPath(pathIndex)}
             />
-            {controlPoints.map(({ points, d }: ControlPoint, commandIndex) => (
+            {controlPoints.map(({ points, d }, commandIndex) => (
               <g key={commandIndex}>
                 <path
                   d={d}
@@ -260,7 +183,7 @@ export const EditSvg = ({
                   stroke={EDIT_CONFIG.color.main}
                   fill={EDIT_CONFIG.fill.default}
                 />
-                {points.map((po: PointObject, k) => {
+                {points.map((po, k) => {
                   const editPathIndex = {
                     path: pathIndex,
                     command: commandIndex,
@@ -287,59 +210,157 @@ export const EditSvg = ({
           </g>
         )
       })}
+    </svg>
+  )
+}
+
+export const EditBoundingBox = ({
+  selected,
+  min,
+  max,
+  onMoveStart,
+  onResizePaths,
+  onResizePathsPreview,
+}: Pick<EditSvgProps, 'onResizePaths' | 'onResizePathsPreview'> & {
+  selected: boolean
+  min: BoundingBox['min']
+  max: BoundingBox['max']
+  onMoveStart: (
+    ev:
+      | React.MouseEvent<SVGRectElement | SVGPathElement | SVGCircleElement>
+      | React.TouchEvent<SVGRectElement | SVGPathElement | SVGCircleElement>
+  ) => void
+}) => {
+  const [resizeBasePoint, setResizeBasePoint] =
+    useState<ResizeBasePoint | null>(null)
+
+  const handleResizeStart = useCallback(
+    (type: FixedPositionType) =>
+      (
+        ev:
+          | React.MouseEvent<SVGRectElement | SVGPathElement | SVGCircleElement>
+          | React.TouchEvent<SVGRectElement | SVGPathElement | SVGCircleElement>
+      ) => {
+        ev.preventDefault()
+        const { x, y } = getPointFromEvent(ev)
+        setResizeBasePoint({
+          type,
+          x,
+          y,
+        })
+      },
+    []
+  )
+
+  const handleResizePreview = useCallback(
+    (ev: MouseEvent | TouchEvent) => {
+      if (!resizeBasePoint) return
+      const { x, y } = getPointFromEvent(ev)
+      const { type, ...base } = resizeBasePoint
+
+      onResizePathsPreview(type, {
+        x: x - base.x,
+        y: y - base.y,
+      })
+    },
+    [onResizePathsPreview, resizeBasePoint]
+  )
+
+  // resize edit
+  const handleResizeEnd = useCallback(
+    (ev: MouseEvent | TouchEvent) => {
+      if (!resizeBasePoint) return
+      const { x, y } = getPointFromEvent(ev)
+      const { type, ...base } = resizeBasePoint
+
+      onResizePaths(type, {
+        x: x - base.x,
+        y: y - base.y,
+      })
+      setResizeBasePoint(null)
+    },
+    [onResizePaths, resizeBasePoint]
+  )
+
+  useEffect(() => {
+    // resizeEdit
+    addEventListener('mouseup', handleResizeEnd)
+    addEventListener('touchcancel', handleResizeEnd)
+
+    // resizePreview
+    addEventListener('mousemove', handleResizePreview)
+    addEventListener('touchmove', handleResizePreview)
+
+    return () => {
+      // resizeEdit
+      removeEventListener('mouseup', handleResizeEnd)
+      removeEventListener('touchcancel', handleResizeEnd)
+
+      // resizePreview
+      removeEventListener('mousemove', handleResizePreview)
+      removeEventListener('touchmove', handleResizePreview)
+    }
+  }, [handleResizeEnd, handleResizePreview])
+
+  return (
+    <>
+      <rect
+        x={min[0]}
+        y={min[1]}
+        width={max[0] - min[0]}
+        height={max[1] - min[1]}
+        stroke={EDIT_CONFIG.color.main}
+        fill={
+          selected ? EDIT_CONFIG.fill.selected : EDIT_CONFIG.fill.boundingBox
+        }
+        onMouseDown={onMoveStart}
+        onTouchStart={onMoveStart}
+      />
       <circle
-        cx={boundingBox.min[0]}
-        cy={boundingBox.min[1]}
+        cx={min[0]}
+        cy={min[1]}
         r={EDIT_CONFIG.point}
         stroke={EDIT_CONFIG.color.main}
         fill={
-          isSelectedBoundingBox
-            ? EDIT_CONFIG.fill.selected
-            : EDIT_CONFIG.fill.boundingBox
+          selected ? EDIT_CONFIG.fill.selected : EDIT_CONFIG.fill.boundingBox
         }
         onMouseDown={handleResizeStart('LeftTop')}
         onTouchStart={handleResizeStart('LeftTop')}
       />
       <circle
-        cx={boundingBox.max[0]}
-        cy={boundingBox.min[1]}
+        cx={max[0]}
+        cy={min[1]}
         r={EDIT_CONFIG.point}
         stroke={EDIT_CONFIG.color.main}
         fill={
-          isSelectedBoundingBox
-            ? EDIT_CONFIG.fill.selected
-            : EDIT_CONFIG.fill.boundingBox
+          selected ? EDIT_CONFIG.fill.selected : EDIT_CONFIG.fill.boundingBox
         }
         onMouseDown={handleResizeStart('RightTop')}
         onTouchStart={handleResizeStart('RightTop')}
       />
       <circle
-        cx={boundingBox.min[0]}
-        cy={boundingBox.max[1]}
+        cx={min[0]}
+        cy={max[1]}
         r={EDIT_CONFIG.point}
         stroke={EDIT_CONFIG.color.main}
         fill={
-          isSelectedBoundingBox
-            ? EDIT_CONFIG.fill.selected
-            : EDIT_CONFIG.fill.boundingBox
+          selected ? EDIT_CONFIG.fill.selected : EDIT_CONFIG.fill.boundingBox
         }
         onMouseDown={handleResizeStart('LeftBottom')}
         onTouchStart={handleResizeStart('LeftBottom')}
       />
       <circle
-        cx={boundingBox.max[0]}
-        cy={boundingBox.max[1]}
+        cx={max[0]}
+        cy={max[1]}
         r={EDIT_CONFIG.point}
         stroke={EDIT_CONFIG.color.main}
         fill={
-          isSelectedBoundingBox
-            ? EDIT_CONFIG.fill.selected
-            : EDIT_CONFIG.fill.boundingBox
+          selected ? EDIT_CONFIG.fill.selected : EDIT_CONFIG.fill.boundingBox
         }
         onMouseDown={handleResizeStart('RightBottom')}
         onTouchStart={handleResizeStart('RightBottom')}
       />
-    </svg>
+    </>
   )
 }
 
