39bc0daf99c8937fa3c9a6ac527580e11a8b66d9
kmkzt <info.pscreator@gmail.com>
2021-12-29 01:35:24 +0900

    fix: Remove eslint-loader

--- examples/docs/next.config.js
+++ examples/docs/next.config.js
@@ -3,22 +3,10 @@ module.exports = {
   basePath: process.env.BASE_PATH || '',
   assetPrefix: process.env.BASE_PATH || '',
   webpack(config, { dev }) {
-    if (dev) {
-      config.module.rules.push({
-        enforce: 'pre',
-        test: /\.[tj]sx?/,
-        exclude: [/node_modules/, /packages/],
-        use: [
-          {
-            loader: 'eslint-loader',
-            options: {
-              fix: true,
-              failOnWarning: false,
-            },
-          },
-        ],
-      })
-    }
+    /**
+     * @todo Add eslint-webpack-plugin.
+     *   https://github.com/webpack-contrib/eslint-webpack-plugin
+     */
     return config
   },
 }
