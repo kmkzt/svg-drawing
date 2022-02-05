49d273d68e8e6d4fce6653ee86dc9c118963b6e9
kmkzt <info.pscreator@gmail.com>
2022-02-06 01:00:14 +0900

    test: Added segmentPointsFromCommand test

--- packages/core/src/edit/segment.test.ts
+++ packages/core/src/edit/segment.test.ts
@@ -1,4 +1,10 @@
-import { calculateCoefficient, segmentPoint } from './segment'
+import {
+  calculateCoefficient,
+  segmentPoint,
+  segmentPointsFromCommand,
+} from './segment'
+import { Curve } from '../svg/command'
+import { Point } from '../svg/point'
 
 describe('BezierCurve calculate point', () => {
   const testData: Parameters<typeof calculateCoefficient>[0] = [
@@ -29,5 +35,22 @@ describe('BezierCurve calculate point', () => {
     expect(
       points.map((p, i) => `${i === 0 ? 'M' : 'L'}${p.x} ${p.y}`).join('')
     ).toMatchSnapshot()
+  })
+
+  describe('segmentPointsFromCommand', () => {
+    it('testData example', () => {
+      expect(segmentPoint(testData)).toEqual(
+        segmentPointsFromCommand(
+          new Curve([
+            new Point(testData[1].x, testData[1].y),
+            new Point(testData[2].x, testData[2].y),
+            new Point(testData[3].x, testData[3].y),
+          ]),
+          {
+            base: new Point(testData[0].x, testData[0].y),
+          }
+        )
+      )
+    })
   })
 })
