b940edbec94741a67ef9bf6171ea6a83398542ee
kmkzt <info.pscreator@gmail.com>
2022-06-03 23:39:49 +0900

    test: add test for drawing update

--- packages/core/src/drawing/drawing.test.ts
+++ packages/core/src/drawing/drawing.test.ts
@@ -8,33 +8,37 @@ const drawMockFactory: DrawFactory = {
   createCommand: createLineCommands,
 }
 
-const noop = () => undefined
-
 describe('Drawing', () => {
   it('Drawing.start', () => {
+    const updateHandler = jest.fn()
     const svg = new Svg({ width: 100, height: 100 })
 
-    const drawing = new Drawing(svg, drawMockFactory, noop)
+    const drawing = new Drawing(svg, drawMockFactory, updateHandler)
     drawing.start()
 
     expect(svg.paths.length).toBe(1)
+    expect(updateHandler).not.toBeCalled()
   })
 
   it('Drawing.dot', () => {
+    const updateHandler = jest.fn()
     const svg = new Svg({ width: 100, height: 100 })
 
-    const drawing = new Drawing(svg, drawMockFactory, noop)
+    const drawing = new Drawing(svg, drawMockFactory, updateHandler)
     drawing.start()
     drawing.dot({ x: 10, y: 10 })
 
     expect(svg.paths.length).toBe(1)
     expect(svg.paths[0].getCommandString()).toEqual('M10 10')
+    expect(updateHandler).toBeCalledTimes(1)
+    expect(updateHandler).toBeCalledWith(svg)
   })
 
   it('Drawing.end', () => {
+    const updateHandler = jest.fn()
     const svg = new Svg({ width: 100, height: 100 })
 
-    const drawing = new Drawing(svg, drawMockFactory, noop)
+    const drawing = new Drawing(svg, drawMockFactory, updateHandler)
     drawing.start()
     drawing.dot({ x: 10, y: 10 })
     drawing.dot({ x: 20, y: 20 })
@@ -48,5 +52,7 @@ describe('Drawing', () => {
     expect(svg.paths.length).toBe(2)
     expect(svg.paths[0].getCommandString()).toEqual('M10 10 l10 10')
     expect(svg.paths[1].getCommandString()).toEqual('M30 30 l10 10')
+    expect(updateHandler).toBeCalledTimes(6)
+    expect(updateHandler).toBeCalledWith(svg)
   })
 })
