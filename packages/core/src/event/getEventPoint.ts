3c50f0ad9fec261080bfaf1b4cc91d25d3b3bee6
kmkzt <info.pscreator@gmail.com>
2023-02-07 00:06:58 +0900

    refactor: remove unuse code

--- packages/core/src/event/getEventPoint.ts
+++ packages/core/src/event/getEventPoint.ts
@@ -1,20 +1,5 @@
 import type { EventPoint } from '../types'
 
-export const SUPPORT_POINTER_EVENT = typeof PointerEvent !== 'undefined'
-
-export const SUPPORT_ON_TOUCH_START = typeof ontouchstart !== 'undefined'
-
-export const SUPPORT_EVENT_LISTENER_PASSIVE_OPTION = (() => {
-  try {
-    const check = () => null
-    addEventListener('testPassive', check, { passive: true })
-    removeEventListener('testPassive', check)
-    return true
-  } catch (e) {
-    return false
-  }
-})()
-
 export const getEventPoint = (
   ev: MouseEvent | TouchEvent | PointerEvent
 ): EventPoint => {
