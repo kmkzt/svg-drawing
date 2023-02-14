39bf7db9079ae361f0443d7df86eff4dfb532817
kmkzt <info.pscreator@gmail.com>
2023-02-15 00:42:55 +0900

    refactor: useResize use useEventHandler

--- packages/react/src/hooks/useResize.ts
+++ packages/react/src/hooks/useResize.ts
@@ -4,18 +4,11 @@ import { useEventHandler } from './useEventHandler'
 import type { UseResize } from '../types'
 
 export const useResize: UseResize = (ref, onResize, active = true) => {
-  const resizeListener = useMemo<ResizeHandler>(() => new ResizeHandler(), [])
+  const handler = useMemo<ResizeHandler>(() => new ResizeHandler(), [])
 
   useEffect(() => {
-    const cleanup = () => resizeListener.cleanup()
+    handler.setHandler(onResize)
+  }, [onResize, handler, active])
 
-    if (!ref.current) return cleanup
-
-    const el = ref.current
-
-    resizeListener.setHandler(onResize)
-    if (active) resizeListener.setup(el)
-
-    return cleanup
-  }, [onResize, resizeListener, ref, active])
+  useEventHandler({ ref, handler, active })
 }
