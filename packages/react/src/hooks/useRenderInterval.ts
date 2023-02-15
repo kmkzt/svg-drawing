1b176218163fc88b11326bbd6b2aebf9d4b6cfe2
kmkzt <info.pscreator@gmail.com>
2023-02-15 22:59:30 +0900

    refactor: refactor useRenderInterval

--- packages/react/src/hooks/useRenderInterval.ts
+++ packages/react/src/hooks/useRenderInterval.ts
@@ -7,20 +7,22 @@ type UpdateCallback = () => void
 export const useRenderInterval = (
   ms: number | undefined = RENDER_INTERVAL
 ): ((update: UpdateCallback) => void) => {
-  const updateCallback = useRef<null | (() => void)>(null)
+  const updateCallback = useRef<UpdateCallback | null>(null)
 
   const render = useCallback((update: UpdateCallback) => {
-    updateCallback.current = () => {
+    const reset = () => {
       updateCallback.current = null
+    }
+
+    updateCallback.current = () => {
+      reset()
       update()
     }
   }, [])
 
   useEffect(() => {
     const stopId = setInterval(() => {
-      if (!updateCallback.current) return
-
-      updateCallback.current()
+      updateCallback.current?.()
     }, ms)
 
     return () => clearInterval(stopId)
