f4f7270e8008462b2215933dfcc2fbb3abbd6a57
kmkzt <info.pscreator@gmail.com>
2023-02-25 00:08:23 +0900

    feat: improve EditPath component

--- packages/react/src/components/EditLayer.tsx
+++ packages/react/src/components/EditLayer.tsx
@@ -2,13 +2,20 @@ import {
   dataBoundingBoxAttributes,
   dataBoundingBoxVertexAttributes,
   dataPathAnchorPointAttributes,
+  dataPathCommandAttributes,
 } from '@svg-drawing/core'
 import React from 'react'
 import { Path } from './Path'
 import { useSvgContext } from './SvgContext'
 import { EDIT_PATH_STYLE } from '../config/editPathStyle'
 import type { BoundingBoxProps } from '../types'
-import type { PointObject, Vertex, EditPathObject } from '@svg-drawing/core'
+import type {
+  Vertex,
+  EditPathObject,
+  ElementKey,
+  EditCommandObject,
+  AnchorPoint,
+} from '@svg-drawing/core'
 
 export const EditLayer = () => {
   const { editProps } = useSvgContext()
@@ -33,21 +40,72 @@ export const EditLayer = () => {
             key={editPath.key}
             elementKey={editPath.key}
             attributes={editPath.attributes}
-            anchorPoints={editPath.anchorPoints}
+            commands={editPath.commands}
           />
         ))}
     </>
   )
 }
 
+const EditAnchorPoint = ({
+  elementKey,
+  anchorPoints,
+  outline,
+}: {
+  elementKey: ElementKey
+  anchorPoints: AnchorPoint[]
+  outline: string | undefined
+}) => (
+  <g>
+    {anchorPoints.map((anchorPoint, i) => (
+      <EditPoint key={i} elementKey={elementKey} anchorPoint={anchorPoint} />
+    ))}
+    {outline ? (
+      <path
+        d={outline}
+        strokeWidth={EDIT_PATH_STYLE.line}
+        stroke={EDIT_PATH_STYLE.color.main}
+        fill={EDIT_PATH_STYLE.fill.default}
+      />
+    ) : null}
+  </g>
+)
+
+const EditCommand = ({
+  elementKey,
+  command: { index, value, selected, anchorPoints, outline },
+}: {
+  elementKey: ElementKey
+  command: EditCommandObject
+}) => (
+  <g>
+    <circle
+      {...dataPathCommandAttributes({ elementKey, commandIndex: index })}
+      r={EDIT_PATH_STYLE.point}
+      cx={value.x}
+      cy={value.y}
+      fill={
+        selected ? EDIT_PATH_STYLE.color.selected : EDIT_PATH_STYLE.color.sub
+      }
+    />
+    {selected ? (
+      <EditAnchorPoint
+        elementKey={elementKey}
+        outline={outline}
+        anchorPoints={anchorPoints}
+      />
+    ) : null}
+  </g>
+)
+
 const EditPath = ({
   elementKey,
   attributes,
-  anchorPoints,
+  commands,
 }: {
   elementKey: EditPathObject['key']
   attributes: EditPathObject['attributes']
-  anchorPoints: EditPathObject['anchorPoints']
+  commands: EditPathObject['commands']
 }) => (
   <g key={elementKey}>
     <Path
@@ -59,59 +117,31 @@ const EditPath = ({
       strokeLinecap={attributes.strokeLinecap}
       strokeLinejoin={attributes.strokeLinejoin}
     />
-    {anchorPoints.outlines.map((d, i) => (
-      <path
-        key={i}
-        d={d}
-        strokeWidth={EDIT_PATH_STYLE.line}
-        stroke={EDIT_PATH_STYLE.color.main}
-        fill={EDIT_PATH_STYLE.fill.default}
-      />
-    ))}
-    {anchorPoints.points.map((po, i) => (
-      <AnchorPoint
-        key={i}
-        point={po.value}
-        pathKey={po.index.path}
-        commandIndex={po.index.command}
-        pointIndex={po.index.point}
-        selected={po.selected}
-      />
+    {commands.map((command, i) => (
+      <EditCommand key={i} elementKey={elementKey} command={command} />
     ))}
   </g>
 )
 
-type AnchorPointProps = {
-  point: PointObject
-  pathKey: string
-  commandIndex: number
-  pointIndex: number
-  selected?: boolean
-}
-
-const AnchorPoint = ({
-  point,
-  pathKey,
-  commandIndex,
-  pointIndex,
-  selected,
-}: AnchorPointProps) => {
-  return (
-    <circle
-      {...dataPathAnchorPointAttributes({
-        elementKey: pathKey,
-        commandIndex,
-        pointIndex,
-      })}
-      cx={point.x}
-      cy={point.y}
-      r={EDIT_PATH_STYLE.point}
-      fill={
-        selected ? EDIT_PATH_STYLE.color.selected : EDIT_PATH_STYLE.color.sub
-      }
-    />
-  )
-}
+const EditPoint = ({
+  elementKey,
+  anchorPoint: { index, selected, value },
+}: {
+  elementKey: ElementKey
+  anchorPoint: AnchorPoint
+}) => (
+  <circle
+    {...dataPathAnchorPointAttributes({
+      elementKey,
+      commandIndex: index.command,
+      pointIndex: index.point,
+    })}
+    cx={value.x}
+    cy={value.y}
+    r={EDIT_PATH_STYLE.point}
+    fill={selected ? EDIT_PATH_STYLE.color.selected : EDIT_PATH_STYLE.color.sub}
+  />
+)
 
 const BoundingBox = ({
   position,
@@ -143,12 +173,13 @@ const BoundingBox = ({
   </>
 )
 
-type BoundingBoxVertexProps = {
+const BoundingBoxVertex = ({
+  vertex,
+  selected,
+}: {
   vertex: Vertex
   selected: boolean
-}
-
-const BoundingBoxVertex = ({ vertex, selected }: BoundingBoxVertexProps) => (
+}) => (
   <circle
     {...dataBoundingBoxVertexAttributes(vertex.type)}
     key={vertex.type}
