6b89437585b57a84e1c6c6f748c4d4cb9c5d66ea
kmkzt <info.pscreator@gmail.com>
2023-04-02 23:34:38 +0900

    test: Added test case to convert element from EditSvgObject

--- packages/core/src/renderer/svgRenderer.test.ts
+++ packages/core/src/renderer/svgRenderer.test.ts
@@ -1,4 +1,6 @@
 import { toElement } from './svgRenderer'
+import { EditSvg } from '../edit/editSvg'
+import { Selector } from '../edit/selector'
 import { createCommand } from '../svg/command'
 import { Path } from '../svg/path'
 import { Svg } from '../svg/svg'
@@ -29,30 +31,166 @@ describe('svgRenderer.ts', () => {
       ])
     )
 
-  it('toElement', () => {
-    const el = toElement({ svg: svg.toJson() })
-    expect(el).toMatchInlineSnapshot(`
-      <svg
-        data-edit-type="frame"
-        height="500"
-        version="1.1"
-        width="500"
-        xmlns="http://www.w3.org/2000/svg"
-        xmlns:xlink="http://www.w3.org/1999/xlink"
-      >
-        <path
-          d="M0 0 c0.2 0.2 0.6 0.8 1 1 c0.4 0.2 0.6 0.2 1 0 c0.4 -0.2 0.8 -0.8 1 -1"
-          data-edit-type="path"
-          data-element-key="p1"
-        />
-        <path
-          d="M4 4 l5 0 l0 4 l-6 -8 z"
-          data-edit-type="path"
-          data-element-key="p2"
-          stroke-linecap="square"
-          stroke-linejoin="miter"
-        />
-      </svg>
-    `)
+  describe('toElement', () => {
+    it('SvgObject convert element', () => {
+      const el = toElement({ svg: svg.toJson() })
+      expect(el).toMatchInlineSnapshot(`
+        <svg
+          data-edit-type="frame"
+          height="500"
+          version="1.1"
+          width="500"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+        >
+          <path
+            d="M0 0 c0.2 0.2 0.6 0.8 1 1 c0.4 0.2 0.6 0.2 1 0 c0.4 -0.2 0.8 -0.8 1 -1"
+            data-edit-type="path"
+            data-element-key="p1"
+          />
+          <path
+            d="M4 4 l5 0 l0 4 l-6 -8 z"
+            data-edit-type="path"
+            data-element-key="p2"
+            stroke-linecap="square"
+            stroke-linejoin="miter"
+          />
+        </svg>
+      `)
+    })
+
+    it('EditSvgObject convert elemet', () => {
+      const selector = new Selector()
+      selector.select({
+        key: 'p1',
+        type: 'path',
+      })
+
+      const el = toElement({
+        svg: svg.toJson(),
+        edit: new EditSvg(svg, selector).toJson(),
+      })
+
+      expect(el).toMatchInlineSnapshot(`
+        <svg
+          data-edit-type="frame"
+          height="500"
+          version="1.1"
+          width="500"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+        >
+          <path
+            d="M0 0 c0.2 0.2 0.6 0.8 1 1 c0.4 0.2 0.6 0.2 1 0 c0.4 -0.2 0.8 -0.8 1 -1"
+            data-edit-type="path"
+            data-element-key="p1"
+          />
+          <path
+            d="M4 4 l5 0 l0 4 l-6 -8 z"
+            data-edit-type="path"
+            data-element-key="p2"
+            stroke-linecap="square"
+            stroke-linejoin="miter"
+          />
+          <rect
+            data-edit-type="bounding-box"
+            fill="rgba(0,0,0,0.1)"
+            height="1.15"
+            stroke="#09f"
+            width="3"
+            x="0"
+            y="0"
+          />
+          <circle
+            cx="0"
+            cy="0"
+            data-edit-type="bounding-box/vertex"
+            data-vertex-type="LeftTop"
+            fill="rgba(0,0,0,0.1)"
+            r="3"
+            stroke="#09f"
+          />
+          <circle
+            cx="3"
+            cy="0"
+            data-edit-type="bounding-box/vertex"
+            data-vertex-type="RightTop"
+            fill="rgba(0,0,0,0.1)"
+            r="3"
+            stroke="#09f"
+          />
+          <circle
+            cx="3"
+            cy="1.15"
+            data-edit-type="bounding-box/vertex"
+            data-vertex-type="RightBottom"
+            fill="rgba(0,0,0,0.1)"
+            r="3"
+            stroke="#09f"
+          />
+          <circle
+            cx="0"
+            cy="1.15"
+            data-edit-type="bounding-box/vertex"
+            data-vertex-type="LeftBottom"
+            fill="rgba(0,0,0,0.1)"
+            r="3"
+            stroke="#09f"
+          />
+          <g>
+            <path
+              data-edit-type="path"
+              data-element-key="p1"
+              fill="none"
+              stroke-width="1"
+            />
+            <g>
+              <circle
+                cx="0"
+                cy="0"
+                data-command-index="0"
+                data-edit-type="path/command"
+                data-element-key="p1"
+                fill="#f90"
+                r="3"
+              />
+            </g>
+            <g>
+              <circle
+                cx="1"
+                cy="1"
+                data-command-index="1"
+                data-edit-type="path/command"
+                data-element-key="p1"
+                fill="#f90"
+                r="3"
+              />
+            </g>
+            <g>
+              <circle
+                cx="2"
+                cy="1"
+                data-command-index="2"
+                data-edit-type="path/command"
+                data-element-key="p1"
+                fill="#f90"
+                r="3"
+              />
+            </g>
+            <g>
+              <circle
+                cx="3"
+                cy="0"
+                data-command-index="3"
+                data-edit-type="path/command"
+                data-element-key="p1"
+                fill="#f90"
+                r="3"
+              />
+            </g>
+          </g>
+        </svg>
+      `)
+    })
   })
 })
