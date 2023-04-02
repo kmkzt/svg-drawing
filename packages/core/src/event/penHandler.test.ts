5dc784cf721b563e9fedda137d718a697ad28401
kmkzt <info.pscreator@gmail.com>
2023-04-02 19:32:54 +0900

    test: Add test for penHandler.cleanup method

--- packages/core/src/event/penHandler.test.ts
+++ packages/core/src/event/penHandler.test.ts
@@ -13,6 +13,7 @@ describe('PenHandler', () => {
     beforeEach(() => {
       jest.resetModules()
     })
+
     it('Add mouseEvent listener when SUPPORT_POINTER_EVENT is false and SUPPORT_TOUCH_ON_START', () => {
       const el = document.createElement('div')
 
@@ -73,6 +74,81 @@ describe('PenHandler', () => {
         penHandler.setup(el)
 
         expect(self.addEventListener).toBeCalledWith(
+          'touchstart',
+          expect.any(Function)
+        )
+      })
+    })
+  })
+
+  describe('cleanup', () => {
+    beforeEach(() => {
+      jest.resetModules()
+    })
+
+    it('Remove mouseEvent listener when SUPPORT_POINTER_EVENT is false and SUPPORT_TOUCH_ON_START', () => {
+      const el = document.createElement('div')
+
+      jest.spyOn(el, 'removeEventListener')
+      jest.spyOn(self, 'removeEventListener')
+      jest.doMock('./support', () => ({
+        SUPPORT_POINTER_EVENT: false,
+        SUPPORT_ON_TOUCH_START: false,
+      }))
+
+      return import('./penHandler').then(({ PenHandler }) => {
+        const draw = setupDrawingMock()
+        const penHandler = new PenHandler(draw)
+        penHandler.setup(el)
+        penHandler.cleanup()
+
+        expect(self.removeEventListener).toBeCalledWith(
+          'mouseup',
+          expect.any(Function)
+        )
+      })
+    })
+
+    it('Remove pointerEvent listener when SUPPORT_POINTER_EVENT is true', () => {
+      const el = document.createElement('div')
+
+      jest.spyOn(el, 'removeEventListener')
+      jest.spyOn(self, 'removeEventListener')
+      jest.doMock('./support', () => ({
+        SUPPORT_POINTER_EVENT: true,
+        SUPPORT_ON_TOUCH_START: false,
+      }))
+
+      return import('./penHandler').then(({ PenHandler }) => {
+        const draw = setupDrawingMock()
+        const penHandler = new PenHandler(draw)
+        penHandler.setup(el)
+        penHandler.cleanup()
+
+        expect(self.removeEventListener).toBeCalledWith(
+          'pointerup',
+          expect.any(Function)
+        )
+      })
+    })
+
+    it('Remove touchEvent listener when SUPPORT_TOUCH_ON_START is true and SUPPORT_POINTER_EVENT is false.', () => {
+      const el = document.createElement('div')
+
+      jest.spyOn(el, 'removeEventListener')
+      jest.spyOn(self, 'removeEventListener')
+      jest.doMock('./support', () => ({
+        SUPPORT_POINTER_EVENT: false,
+        SUPPORT_ON_TOUCH_START: true,
+      }))
+
+      return import('./penHandler').then(({ PenHandler }) => {
+        const draw = setupDrawingMock()
+        const penHandler = new PenHandler(draw)
+        penHandler.setup(el)
+        penHandler.cleanup()
+
+        expect(self.removeEventListener).toBeCalledWith(
           'touchstart',
           expect.any(Function)
         )
