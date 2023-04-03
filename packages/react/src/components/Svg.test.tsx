2886ddf6601e5514f6d7528b8f602ebc8ec472fb
kmkzt <info.pscreator@gmail.com>
2023-04-03 23:27:23 +0900

    test: Remove legacyRoot options

--- packages/react/src/components/Svg.test.tsx
+++ packages/react/src/components/Svg.test.tsx
@@ -8,10 +8,7 @@ const BaseComponent = (props: ComponentProps<typeof Svg>) => <Svg {...props} />
 describe('Svg', () => {
   it('Render background', () => {
     const { container } = render(
-      <BaseComponent width={500} height={500} background="red" elements={[]} />,
-      {
-        legacyRoot: true,
-      }
+      <BaseComponent width={500} height={500} background="red" elements={[]} />
     )
 
     expect(container.querySelector('rect')?.getAttribute('fill')).toBe('red')
