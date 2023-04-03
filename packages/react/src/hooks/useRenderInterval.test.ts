960c84baa184e840d10deb847092bd894a388e97
kmkzt <info.pscreator@gmail.com>
2023-04-03 23:50:00 +0900

    test: Fix useRenderInterval.testt.tsx

--- packages/react/src/hooks/useRenderInterval.test.ts
+++ packages/react/src/hooks/useRenderInterval.test.ts
@@ -1,4 +1,4 @@
-import { act, renderHook } from '@testing-library/react-hooks'
+import { act, renderHook } from '@testing-library/react'
 import { useRenderInterval } from './useRenderInterval'
 
 describe('useRenderInterval', () => {
