be9e507650d16eb9bd882aaca3fddffec4ce3018
kmkzt <info.pscreator@gmail.com>
2023-04-04 23:06:42 +0900

    test: Fix drawing.test.tsx

--- packages/core/src/drawing/drawing.test.ts
+++ packages/core/src/drawing/drawing.test.ts
@@ -2,7 +2,7 @@ import { createLineCommands } from './convert'
 import { Drawing } from './drawing'
 import { Path } from '../svg/path'
 import { Svg } from '../svg/svg'
-import type { DrawFactory } from '../types'
+import type { DrawFactory, RenderParams } from '../types'
 
 const drawMockFactory: DrawFactory = {
   createElement: () => new Path({}),
@@ -12,35 +12,52 @@ const drawMockFactory: DrawFactory = {
 
 describe('Drawing', () => {
   it('Drawing.start', () => {
-    const updateHandler = jest.fn()
+    const render = jest.fn()
     const svg = new Svg({ width: 100, height: 100 })
+    const drawing = new Drawing(svg, drawMockFactory, render)
 
-    const drawing = new Drawing(svg, drawMockFactory, updateHandler)
     drawing.start()
 
     expect(svg.elements.length).toBe(1)
-    expect(updateHandler).not.toBeCalled()
+    expect(render).not.toBeCalled()
   })
 
   it('Drawing.dot', () => {
-    const updateHandler = jest.fn()
+    const render = jest.fn()
     const svg = new Svg({ width: 100, height: 100 })
+    const drawing = new Drawing(svg, drawMockFactory, render)
 
-    const drawing = new Drawing(svg, drawMockFactory, updateHandler)
     drawing.start()
     drawing.dot({ x: 10, y: 10 })
 
+    const expected: RenderParams = {
+      svg: {
+        background: undefined,
+        elements: [
+          {
+            attributes: {
+              d: 'M10 10',
+            },
+            key: 'p1',
+            type: 'path',
+          },
+        ],
+        height: 100,
+        width: 100,
+      },
+    }
+
     expect(svg.elements.length).toBe(1)
     expect(svg.elements[0].getCommandString()).toEqual('M10 10')
-    expect(updateHandler).toBeCalledTimes(1)
-    expect(updateHandler).toBeCalledWith(svg)
+    expect(render).toBeCalledTimes(1)
+    expect(render).toBeCalledWith(expected)
   })
 
   it('Drawing.end', () => {
-    const updateHandler = jest.fn()
+    const render = jest.fn()
     const svg = new Svg({ width: 100, height: 100 })
+    const drawing = new Drawing(svg, drawMockFactory, render)
 
-    const drawing = new Drawing(svg, drawMockFactory, updateHandler)
     drawing.start()
     drawing.dot({ x: 10, y: 10 })
     drawing.dot({ x: 20, y: 20 })
@@ -51,10 +68,34 @@ describe('Drawing', () => {
     drawing.dot({ x: 40, y: 40 })
     drawing.end()
 
+    const expected: RenderParams = {
+      svg: {
+        background: undefined,
+        elements: [
+          {
+            attributes: {
+              d: 'M10 10 l10 10',
+            },
+            key: 'p1',
+            type: 'path',
+          },
+          {
+            attributes: {
+              d: 'M30 30 l10 10',
+            },
+            key: 'p2',
+            type: 'path',
+          },
+        ],
+        height: 100,
+        width: 100,
+      },
+    }
+
     expect(svg.elements.length).toBe(2)
     expect(svg.elements[0].getCommandString()).toEqual('M10 10 l10 10')
     expect(svg.elements[1].getCommandString()).toEqual('M30 30 l10 10')
-    expect(updateHandler).toBeCalledTimes(4)
-    expect(updateHandler).toBeCalledWith(svg)
+    expect(render).toBeCalledTimes(4)
+    expect(render).toBeCalledWith(expected)
   })
 })
